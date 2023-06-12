b3d27238b819244a673692d162f2bf18fd1a9ce1
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-06-12 19:22:12 -0400

    Create client.js
    Initial implementation of client.js
    Establish connection and send player name in client.js
    Export connect function from client.js

---
+++ client.js
@@ -0,0 +1,23 @@
+// establishes a connection with the game server
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  conn.on('data' , (data) => {
+    console.log('Server says:', data);
+  });
+  conn.on('connect' ,() => {
+    console.log('Successfully connected to game server')
+    conn.write('Name: KDL')
+  })
+  return conn;
+};
+module.exports = {
+  connect
+};
