1cc1970a69647813c77f1a8275275089eae95e9b
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-06-13 21:08:16 -0400

    Refactor constants.js
    Added anonymous function to the e key in constants object
    function takes previous keybinded taunts and randomly selects one

--- constants.js
+++ constants.js
@@ -6,9 +6,19 @@ const constants = {
   '\u0073': 'Move: down',
   '\u0077': 'Move: up',
   '\u0064': 'Move: right',
-  '\u0065': 'Say: AYE IM WALKIN HERE',
-  '\u0072': 'Say: GG',
-  '\u0074': 'Say: Free Armor Trimming',
-  '\u0071': 'Say: Jungle Diff'
-}
+  '\u0065': function() {
+    const taunts = ['Say: AYE IM WALKIN HERE', 'Say: GG', 'Say: Free Armor Trimming', 'Say: Jungle Diff'];
+    const randomIndex = Math.floor(Math.random() * taunts.length);
+    const randomTaunt = taunts[randomIndex];
+    return randomTaunt;
+  }
+};
+
 module.exports = constants;
+
+
+
+// '\u0065': 'Say: AYE IM WALKIN HERE',
+// '\u0072': 'Say: GG',
+// '\u0074': 'Say: Free Armor Trimming',
+// '\u0071': 'Say: Jungle Diff'
