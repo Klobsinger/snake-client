02636043853cf6ee69668395f47e700365868149
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-06-13 20:31:03 -0400

    Create constants.js
    create constants object which holds key value pairs for values that dont change and can be passed around the program

---
+++ constants.js
@@ -0,0 +1,14 @@
+const constants = {
+  IP: "localhost",
+  PORT: 50541,
+  Name: 'Name: KDL',
+  '\u0061': 'Move: left',
+  '\u0073': 'Move: down',
+  '\u0077': 'Move: up',
+  '\u0064': 'Move: right',
+  '\u0065': 'Say: AYE IM WALKIN HERE',
+  '\u0072': 'Say: GG',
+  '\u0074': 'Say: Free Armor Trimming',
+  '\u0071': 'Say: Jungle Diff'
+}
+module.exports = constants;
